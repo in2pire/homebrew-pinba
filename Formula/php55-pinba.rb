e01b4b46b7ac97683f446b6f33db6db7948f66ad
Nhat Tran <thmnhat@gmail.com>
2014-10-20 19:16:22 +0700

    Finish pinball extension for other PHP versions

---
+++ Formula/php55-pinba.rb
@@ -0,0 +1,23 @@
+require File.expand_path("../../../../../Taps/homebrew/homebrew-php/Abstract/abstract-php-extension", __FILE__)
+
+class Php55Pinba < AbstractPhp55Extension
+  init
+  homepage 'https://github.com/tony2001/pinba_extension'
+  url 'https://github.com/tony2001/pinba_extension/archive/master.tar.gz'
+  sha1 '063f1005f4d49a99d40c210476978acfa6d21b74'
+  version 'rev-4f20a81a0c'
+  head 'https://github.com/tony2001/pinba_extension.git'
+
+  depends_on 'pinba'
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/pinba.so"
+    write_config_file if build.with? "config-file"
+  end
+end
